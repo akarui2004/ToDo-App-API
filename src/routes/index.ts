340f7030066f9f286e87e424a3941a04335e69dc
Chan Dinh <dinhhoangbaochan@gmail.com>
2023-02-26 20:31:04 +0700

    feat: init api and route to display all priorities

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import helmet from "helmet";
 import { allowCors } from "src/middlewares";
 import public0 from "./public";
+import prioritiesRouter from './api/priorities';
 
 const router = Router();
 
@@ -9,5 +10,8 @@ router.use(helmet(), allowCors());
 
 // note: public routes doesn't have /public prefix
 router.use(public0);
+
+// api routes with /api prefix
+router.use('/api', prioritiesRouter);
 
 export default router;
